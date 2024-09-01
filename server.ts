7d26fdb21967071b87a26d4adac33a1555bfc5eb
Shyam <mshayam41@gmail.com>
2024-09-01 10:04:42 +0530

    Env working

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import unocss from "aleph/plugins/unocss";
 import config from "./unocss.config.ts";
 import modules from "./routes/_export.ts";
 import {load} from "load-env"
-//console.log(await load({export: true}));
+//console.log(await load({export: true})); load the environ mental variables in entry point then only it works
 export const config2 = load({export: true})
 
 serve({
